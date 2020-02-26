ecc87d7ae42de5ecb7ce19775c6d546318db148e
fran9641 <fran.cs9641@gmail.com>
2020-02-26 10:33:08 -0600

    ajaxcambios

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", e=>{
-    const form = document.querySelector("#frmConversores");
-    form.addEventListener("submit", event=>{
-        event.preventDefault();
-  
-       let nombre = document.querySelector("#txtNombre").Value;
-       fetch(`http://localhost:8080/Programacionpractica-conversores/saludo.php?nombre=${nombre}`)
-            .then(resp=>resp.text())
-            .then(resp=>{
-                document.querySelector("#lblRespuesta").innerHTML = respuesta;
+  const form = document.querySelector("#frmSaludo");
+  form.addEventListener("submit", event=>{
+      event.preventDefault();
 
-            });
-    });
-   });
+     let nombre = document.querySelector("#txtNombre").value;
+     fetch(`saludo.php?name=${nombre}`)
+          .then(resp=>resp.text())
+          .then(respuesta=>{
+              document.querySelector("#lblSaludo").innerHTML = respuesta;
+          });
+  });
+});
